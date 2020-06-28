2a0933c0c2a5e91a54f7f8f44cccb29e90da619d
tiisachenko <tiisachenko@dasreda.ru>
2020-06-28 09:45:31 +0300

    Own version of set of microservices based on Spring Cloud + Kafka Made by book "Learn Microservices with Spring Boot: A Practical Approach to RESTful Services using RabbitMQ, Eureka, Ribbon, Zuul and Cucumber"

---
+++ short-url/src/test/java/com/timurisachenko/shorturl/ShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timurisachenko.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
